aeb30319f6b4e40a843872cae1f007b5f472e9eb
Hemant Jha <156426952+DevHemantJha@users.noreply.github.com>
2024-07-04 13:41:14 +0530

    User Region Mapping API added.

---
+++ backend-APIs/controllers/mapping.controller.js
@@ -0,0 +1,32 @@
+import UserRegionMappingModel from "../models/user_region_mapping.js";
+
+
+const userRegionMapping = async (req, res) => {
+    try {
+        const data = req.body;
+        if (!Array.isArray(data)) {
+            return res.status(400).json({ error: 'Invalid data format, expected an array' });
+        }
+
+        await UserRegionMappingModel.insertMany(data);
+        res.status(201).json({ message: 'Data added successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred', details: error.message });
+    }
+};
+const getUserRegion = async (req, res) => {
+    try {
+        const userID = req.params.userID;
+        if (!userID) {
+          return res.status(400).json({ error: 'User ID is required' });
+        }
+    
+        const regions = await UserRegionMappingModel.find({ user_id: userID }, 'region -_id');
+        res.status(200).json(regions.map(record => record.region));
+      } catch (error) {
+        res.status(500).json({ error: 'An error occurred', details: error.message });
+      }
+};
+
+
+export { userRegionMapping, getUserRegion };
---
+++ backend-APIs/models/user_region_mapping.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userRegionMappingSchema = new mongoose.Schema({
+    userID: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    region: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    isActive: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
+}, {
+    timestamps: true
+});
+
+const UserRegionMappingModel = mongoose.model("user_region_mapping", userRegionMappingSchema);
+
+export default UserRegionMappingModel;
